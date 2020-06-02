2ffb8535f8508239239bf5fa6177373a09ee2ede
keathal <jemovey@gmail.com>
2020-06-02 18:29:27 +0700

    camera controller

---
+++ New Unity Project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public float speedTr = 150.0f;
+    Vector3 moveVector;
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetKey(KeyCode.Q) ? -1 : (Input.GetKey(KeyCode.E) ? 1 : 0);
+        moveVector.z = Input.GetAxisRaw("Vertical");
+        moveVector = transform.TransformVector(moveVector);
+        controller.Move(moveVector * Time.deltaTime * speedTr);
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        if (pitch < 0.0f)
+            pitch = 0;
+        if (pitch > 90.0f)
+            pitch = 90;
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+    }
+}
